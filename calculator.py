d3463c1118c37e72de7b0ea5d4334257e5016cff
rizwana1906 <137092918+rizwana1906@users.noreply.github.com>
2023-10-22 19:36:46 +0530

    Add files via upload
    This Python program is like a digital calculator. You can use it to do simple math like adding, subtracting, multiplying, and dividing. It shows you a menu where you choose what kind of math you want to do. Then, you enter the numbers, and it gives you the answer. It's a handy tool for doing basic math calculations.

---
+++ calculator.py
@@ -0,0 +1,48 @@
+# Function for addition
+def add(x, y):
+    return x + y
+
+# Function for subtraction
+def subtract(x, y):
+    return x - y
+
+# Function for multiplication
+def multiply(x, y):
+    return x * y
+
+# Function for division
+def divide(x, y):
+    if y == 0:
+        return "Cannot divide by zero"
+    return x / y
+
+# Main calculator function
+def calculator():
+    print("Basic Calculator that can perform arithmetic operations with only two numbers!")
+    print("Options to perform operations:")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+
+    choice = input("Enter choice (1/2/3/4): ")
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if choice == '1':
+        result = add(num1, num2)
+    elif choice == '2':
+        result = subtract(num1, num2)
+    elif choice == '3':
+        result = multiply(num1, num2)
+    elif choice == '4':
+        result = divide(num1, num2)
+    else:
+        result = "Invalid input"
+
+    print(f"Result: {result}")
+
+# Run the calculator
+if __name__ == "__main__":
+    calculator()
